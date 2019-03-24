e35a8770c1aa7f8938387a426e7544e3d6d1a9b1
Theo B <me@theos.space>
2019-03-24 05:50:21 -0700

    fix assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require_tree .
 
 
-$(() => {
+$(function() {
   if ($('.galleria').length) {
     Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/themes/classic/galleria.classic.min.js')
     Galleria.run('.galleria')
