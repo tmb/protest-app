bd2940fe9665b8c22130604c1be23f21d79c5662
Theo B <me@theos.space>
2019-03-23 21:56:11 -0700

    cool!

--- app/models/protest.rb
+++ app/models/protest.rb
@@ -4,17 +4,10 @@ class Protest < ApplicationRecord
 
   has_many :rsvp_people
 
-  before_save :update_lat_long
+  geocoded_by :location
+  after_validation :geocode
 
   def protestors
     rsvp_people
   end 
-
-  private
-
-  def update_lat_long
-    coords = Geocoder.search(self.location).first.coordinates
-    self.latitude = coords[0]
-    self.longitude = coords[1]
-  end
 end
